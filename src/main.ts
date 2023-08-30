d8e5d3e7076038a0efb54175780bb4d124e5f526
adlipeeters <adlipeeters@gmail.com>
2023-08-30 19:35:44 +0300

    bugs fixing

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   app.use(express.static(path.join(__dirname, '../frontend/build')));
 
   // And then the catch-all:
-  app.get('*', (req: any, res: any) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
   });
 }
