7528a2c74bb8a2eefcb2fd293099ef262d3d6e62
adlipeeters <adlipeeters@gmail.com>
2023-08-30 20:57:42 +0300

    workflow refactoring

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   await app.listen(3000);
-  // console.log('api')
+  // console.log('apii')
 }
 bootstrap();
