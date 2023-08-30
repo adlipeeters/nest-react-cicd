b92b72973c07ce4cbbf0ac4c58441ab5e2d8ce7b
adlipeeters <adlipeeters@gmail.com>
2023-08-30 19:28:52 +0300

    project initialization

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as express from 'express';
+import * as path from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // And then the catch-all:
+  app.get('*', (req: any, res: any) => {
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+  });
+}
+bootstrap();
